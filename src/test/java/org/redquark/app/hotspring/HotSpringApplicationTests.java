a0e845f50b926e33e7fca6514868cbdd7c340ba8
Anirudh Sharma <anirudh03sharma@gmail.com>
2021-11-25 18:52:10 +0530

    Added project skeleton

---
+++ src/test/java/org/redquark/app/hotspring/HotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.redquark.app.hotspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
